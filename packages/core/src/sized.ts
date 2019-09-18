6b137072b9087e722365b964f4191d74614b5e90
Dan Freeman <dfreeman@salsify.com>
2019-09-18 21:39:22 +0200

    Establish `DataSource` protocol

---
+++ packages/core/src/sized.ts
@@ -0,0 +1,39 @@
+declare const Unique: unique symbol;
+export type Unique<T, Tag> = T & { [Unique]: Tag };
+
+// These utilities help prevent silly, hard-to-track-down
+// mistakes like reading one byte as an opcode operand
+// when it expects only a word.
+
+/**
+ * A JS number certified to be 0 or 1;
+ */
+export type Bit = Unique<number, 'bit'> | 0 | 1;
+
+/**
+ * A JS number certified to represent a byte.
+ */
+export type Byte = Unique<number, 'byte'> | 0 | 1 | 0xff;
+
+/**
+ * A JS number certified to represent a 16-bit word.
+ */
+export type Word = Unique<number, 'word'> | 0 | 1 | 0xffff;
+
+export function bit(value: number | boolean): Bit {
+  return (+!!value & 0x1) as Bit;
+}
+
+/**
+ * Shortens the given number to one byte.
+ */
+export function byte(value: number): Byte {
+  return (value & 0xff) as Byte;
+}
+
+/**
+ * Shortens the given number to two bytes.
+ */
+export function word(value: number): Word {
+  return (value & 0xffff) as Word;
+}
---
+++ packages/core/tests/data-source.test.ts
@@ -0,0 +1,113 @@
+import { nil, buffer, registerBank } from '../src/data-source';
+
+describe('DataSource', () => {
+  describe('nil', () => {
+    test('returns 0 on read', () => {
+      let dataSource = nil();
+      expect(dataSource.readByte(0)).toBe(0);
+      expect(dataSource.readByte(0xffff)).toBe(0);
+    });
+
+    test('ignores writes', () => {
+      let dataSource = nil();
+      dataSource.writeByte(0, 0xff);
+      expect(dataSource.readByte(0)).toBe(0);
+    });
+  });
+
+  describe('buffer', () => {
+    test('reflects reads and writes in the underlying buffer', () => {
+      let dataSource = buffer(2);
+
+      expect(dataSource.readByte(0)).toBe(0);
+      expect(dataSource.readByte(1)).toBe(0);
+      expect(dataSource.readByte(2)).toBe(undefined);
+      expect([...dataSource.buffer]).toEqual([0, 0]);
+
+      dataSource.writeByte(1, 0xff);
+
+      expect(dataSource.readByte(0)).toBe(0);
+      expect(dataSource.readByte(1)).toBe(0xff);
+      expect([...dataSource.buffer]).toEqual([0, 0xff]);
+
+      dataSource.buffer[0] = 0xf0;
+
+      expect(dataSource.readByte(0)).toBe(0xf0);
+      expect(dataSource.readByte(1)).toBe(0xff);
+      expect([...dataSource.buffer]).toEqual([0xf0, 0xff]);
+    });
+
+    test('reflects data in a pre-existing buffer', () => {
+      let underlying = new Uint8Array(2);
+      let dataSource = buffer(underlying);
+
+      underlying[0] = 0xfe;
+      underlying[1] = 0xef;
+
+      expect(dataSource.readByte(0)).toBe(0xfe);
+      expect(dataSource.readByte(1)).toBe(0xef);
+
+      dataSource.writeByte(0, 0x01);
+      dataSource.writeByte(1, 0xff);
+
+      expect(underlying[0]).toBe(0x01);
+      expect(underlying[1]).toBe(0xff);
+    });
+  });
+
+  describe('registerBank', () => {
+    test('allows access via address or name', () => {
+      let dataSource = registerBank([{ address: 0, name: 'zero' }, { address: 1, name: 'one' }]);
+
+      expect(dataSource.readByte(0)).toBe(0);
+      expect(dataSource.readByte(1)).toBe(0);
+      expect(dataSource.zero).toBe(0);
+      expect(dataSource.one).toBe(0);
+
+      dataSource.writeByte(0, 0xff);
+
+      expect(dataSource.readByte(0)).toBe(0xff);
+      expect(dataSource.readByte(1)).toBe(0);
+      expect(dataSource.zero).toBe(0xff);
+      expect(dataSource.one).toBe(0);
+
+      dataSource.one = 1;
+
+      expect(dataSource.readByte(0)).toBe(0xff);
+      expect(dataSource.readByte(1)).toBe(1);
+      expect(dataSource.zero).toBe(0xff);
+      expect(dataSource.one).toBe(1);
+    });
+  });
+
+  test('restricts writable bits when a `writeMask` is specified', () => {
+    let dataSource = registerBank([{ address: 0, name: 'register', writeMask: 0b11000001 }]);
+
+    expect(dataSource.readByte(0)).toBe(0);
+    expect(dataSource.register).toBe(0);
+
+    // Set all bits
+    dataSource.register = 0xff;
+
+    expect(dataSource.readByte(0)).toBe(0xff);
+    expect(dataSource.register).toBe(0xff);
+
+    // Writing all zeroes only clears bits 0, 6 and 7
+    dataSource.writeByte(0, 0);
+
+    expect(dataSource.readByte(0)).toBe(0b00111110);
+    expect(dataSource.register).toBe(0b00111110);
+
+    // Clear all bits
+    dataSource.register = 0;
+
+    expect(dataSource.readByte(0)).toBe(0);
+    expect(dataSource.register).toBe(0);
+
+    // Writing all ones only set bits 0, 6 and 7
+    dataSource.writeByte(0, 0xff);
+
+    expect(dataSource.readByte(0)).toBe(0b11000001);
+    expect(dataSource.register).toBe(0b11000001);
+  });
+});
