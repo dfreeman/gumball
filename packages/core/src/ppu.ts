31fa058893c559ee2802daa326f9cf490f89fe85
Dan Freeman <dfreeman@salsify.com>
2020-08-23 22:20:10 +0200

    Add a skeleton for the PPU
    It doesn't actually do any graphics processing yet,
    but it at least tracks the passage of time and sets
    the mode and coincidence flags in STAT, which is
    enough to make it through the boot ROM.

---
+++ packages/core/src/ppu.ts
@@ -0,0 +1,146 @@
+import { registerBank } from './data-source/register-bank';
+import { buffer } from './data-source/buffer';
+import { guarded } from './data-source/guarded';
+import { byte, bit, Byte } from './utils/sized-numbers';
+
+/**
+ * The Picture Processing Unit acts as the interface between
+ * the LCD and the rest of the system.
+ */
+export default class PPU {
+  public readonly ram = guarded(buffer(0x2000));
+  public readonly oam = guarded(buffer(0x009f));
+  public readonly registers = registerBank([
+    // https://gbdev.io/pandocs/#ff40-lcd-control-register
+    { address: 0x00, name: 'lcdc' },
+
+    // https://gbdev.io/pandocs/#lcd-status-register
+    { address: 0x01, name: 'stat', writeMask: 0b01111000 },
+
+    // https://gbdev.io/pandocs/#lcd-position-and-scrolling
+    { address: 0x02, name: 'scy' },
+    { address: 0x03, name: 'scx' },
+    { address: 0x04, name: 'ly', writeMask: 0 },
+    { address: 0x05, name: 'lyc' },
+    { address: 0x0a, name: 'wy' },
+    { address: 0x0b, name: 'wx' },
+
+    // https://gbdev.io/pandocs/#lcd-monochrome-palettes
+    { address: 0x07, name: 'bgp' },
+    { address: 0x08, name: 'obp0' },
+    { address: 0x09, name: 'obp1' },
+
+    // https://gbdev.io/pandocs/#lcd-oam-dma-transfers
+    { address: 0x06, name: 'dma' },
+  ]);
+
+  private frameClocks = 0;
+  private lineClocks = 0;
+  private mode = PPUMode.OAMSearch;
+
+  public constructor() {
+    // Ensure memory locks and mode calculations start in the correct state.
+    this.step(0);
+  }
+
+  public step(clocks: number): void {
+    let registers = this.registers.values;
+    let lineIncrement = Math.floor((this.lineClocks + clocks) / LINE_DURATION);
+
+    this.frameClocks = (this.frameClocks + clocks) % FRAME_DURATION;
+    this.lineClocks = (this.lineClocks + clocks) % LINE_DURATION;
+    this.mode = this.computeMode();
+
+    if (lineIncrement) {
+      registers.ly = byte((registers.ly + lineIncrement) % TOTAL_LINES);
+    }
+
+    registers.stat = byte(
+      (registers.stat & 0b01111000) | (bit(registers.lyc === registers.ly) << 2) | (this.mode & 0b00000011),
+    );
+
+    if (this.mode === PPUMode.Drawing || this.mode === PPUMode.OAMSearch) {
+      this.oam.lock(0xff);
+    } else {
+      this.oam.unlock();
+    }
+
+    if (this.mode === PPUMode.Drawing) {
+      this.ram.lock(0xff);
+    } else {
+      this.ram.unlock();
+    }
+  }
+
+  private computeMode(): PPUMode {
+    let { frameClocks, lineClocks } = this;
+    if (frameClocks >= LINE_DURATION * VISIBLE_LINES) {
+      return PPUMode.VBlank;
+    }
+
+    if (lineClocks < DRAWING_START) {
+      return PPUMode.OAMSearch;
+    } else if (lineClocks < HBLANK_START) {
+      return PPUMode.Drawing;
+    } else {
+      return PPUMode.HBlank;
+    }
+  }
+}
+
+export const DRAWING_START = 80;
+export const HBLANK_START = 252;
+
+export const VISIBLE_LINES = 144;
+export const TOTAL_LINES = 154;
+
+export const LINE_DURATION = 456;
+export const FRAME_DURATION = LINE_DURATION * TOTAL_LINES;
+
+// https://gbdev.io/pandocs/#ff41-stat-lcdc-status-r-w
+// https://gbdev.io/pandocs/#pixel-fifo
+// Note that 1 'dot' below corresponds to one 'clock tick'
+// on the DMG (or 1/4 of a cycle), but would be 2 clocks
+// on CGB in double-speed mode.
+export enum PPUMode {
+  /**
+   * Horizontal blanking.
+   *
+   * Lasts 85 to 208 dots, i.e. 376 - however long mode 3 took.
+   * During that time both OAM and VRAM are available to the CPU.
+   */
+  HBlank = 0,
+
+  /**
+   * Vertical blanking.
+   *
+   * Lasts 4560 dots, during which both OAM and VRAM are available
+   * to the CPU.
+   */
+  VBlank = 1,
+
+  /**
+   * Scanning OAM for (X, Y) coordinates of sprites that overlap this line.
+   *
+   * Lasts 80 dots, during which VRAM is available but OAM is not.
+   */
+  OAMSearch = 2,
+
+  /**
+   * Reading OAM and VRAM to generate the picture.
+   *
+   * Lasts 168 to 291 dots, depending on number of sprites being drawn.
+   * During that time, neither OAM nor VRAM is available to the CPU.
+   *
+   * Three things are known to contribute to extending the duration of mode 3:
+   *  - If SCX % 8 is not 0 at the beginning of a scanline, rendering is paused
+   *    for that many dots while pixels from the leftmost tile are discarded.
+   *  - An active window causes a pause of at least 6 dots
+   *  - Each sprite usually causes a pause of `11 - min(5, (x + SCX) % 8)` dots
+   *
+   * Currently the PPU makes no attempt to emulate this timing, and if that
+   * ever changes much more information will need to be gleaned from pandocs
+   * and elsewhere.
+   */
+  Drawing = 3,
+}
---
+++ packages/core/tests/ppu.test.ts
@@ -0,0 +1,148 @@
+import PPU, { PPUMode } from '../src/ppu';
+import * as Constants from '../src/ppu';
+
+describe('PPU', () => {
+  describe('STAT', () => {
+    test('mode bits', () => {
+      let ppu = new PPU();
+
+      // We start in OAMSearch
+      expect(ppu.registers.values.stat & 0b11).toEqual(PPUMode.OAMSearch);
+
+      // Step to the last moment of OAMSearch
+      ppu.step(Constants.DRAWING_START - 1);
+      expect(ppu.registers.values.stat & 0b11).toEqual(PPUMode.OAMSearch);
+
+      // Step from the end of OAMSearch to Drawing
+      ppu.step(1);
+      expect(ppu.registers.values.stat & 0b11).toEqual(PPUMode.Drawing);
+
+      // Step to the last moment of Drawing
+      ppu.step(Constants.HBLANK_START - Constants.DRAWING_START - 1);
+      expect(ppu.registers.values.stat & 0b11).toEqual(PPUMode.Drawing);
+
+      // Step from the end of Drawing to HBlank
+      ppu.step(1);
+      expect(ppu.registers.values.stat & 0b11).toEqual(PPUMode.HBlank);
+
+      // Step to the last moment of HBlank
+      ppu.step(Constants.LINE_DURATION - Constants.HBLANK_START - 1);
+      expect(ppu.registers.values.stat & 0b11).toEqual(PPUMode.HBlank);
+
+      // Step from the end of HBlank and come around to OAMSearch for the next scanline
+      ppu.step(1);
+      expect(ppu.registers.values.stat & 0b11).toEqual(PPUMode.OAMSearch);
+
+      // Step through the last moment of HBlank on the last visible scanline
+      ppu.step(Constants.LINE_DURATION * (Constants.VISIBLE_LINES - 1) - 1);
+      expect(ppu.registers.values.stat & 0b11).toEqual(PPUMode.HBlank);
+
+      // Step ino VBlank
+      ppu.step(1);
+      expect(ppu.registers.values.stat & 0b11).toEqual(PPUMode.VBlank);
+
+      // Step through the 10 offscreen scanlines of VBlank
+      ppu.step(Constants.LINE_DURATION * (Constants.TOTAL_LINES - Constants.VISIBLE_LINES) - 1);
+      expect(ppu.registers.values.stat & 0b11).toEqual(PPUMode.VBlank);
+
+      // Come all the way around to OAMSearch for the first scanline of the next frame
+      ppu.step(1);
+      expect(ppu.registers.values.stat & 0b11).toEqual(PPUMode.OAMSearch);
+    });
+
+    test('coincidence bit', () => {
+      let ppu = new PPU();
+
+      // LY and LYC both start at 0, so they coincide
+      expect(ppu.registers.values.stat & 0b0100).toBeTruthy();
+
+      // Change LYC and verify the coincidence bit is cleared
+      ppu.registers.values.lyc = 1;
+      ppu.step(1);
+      expect(ppu.registers.values.stat & 0b0100).toBeFalsy();
+
+      // Proceed to the very end of line 0 and check the bit is still cleared
+      ppu.step(Constants.LINE_DURATION - 2);
+      expect(ppu.registers.values.stat & 0b0100).toBeFalsy();
+
+      // Then step into line 1 and verify the coincidence bit is set again
+      ppu.step(1);
+      expect(ppu.registers.values.stat & 0b0100).toBeTruthy();
+    });
+  });
+
+  describe('memory access', () => {
+    test('during OAMSearch', () => {
+      let ppu = new PPU();
+
+      expect(ppu.registers.values.stat & 0b11).toEqual(PPUMode.OAMSearch);
+
+      // RAM is unlocked
+      expect(ppu.ram.readByte(0)).toBe(0);
+      ppu.ram.writeByte(0, 1);
+      expect(ppu.ram.readByte(0)).toBe(1);
+
+      // OAM is locked
+      expect(ppu.oam.inner.readByte(0)).toBe(0);
+      expect(ppu.oam.readByte(0)).toBe(0xff);
+      ppu.oam.writeByte(0, 1);
+      expect(ppu.oam.inner.readByte(0)).toBe(0);
+      expect(ppu.oam.readByte(0)).toBe(0xff);
+    });
+
+    test('during Drawing', () => {
+      let ppu = new PPU();
+
+      ppu.step(Constants.DRAWING_START);
+      expect(ppu.registers.values.stat & 0b11).toEqual(PPUMode.Drawing);
+
+      // RAM is locked
+      expect(ppu.ram.inner.readByte(0)).toBe(0);
+      expect(ppu.ram.readByte(0)).toBe(0xff);
+      ppu.ram.writeByte(0, 1);
+      expect(ppu.ram.inner.readByte(0)).toBe(0);
+      expect(ppu.ram.readByte(0)).toBe(0xff);
+
+      // OAM is locked
+      expect(ppu.oam.inner.readByte(0)).toBe(0);
+      expect(ppu.oam.readByte(0)).toBe(0xff);
+      ppu.oam.writeByte(0, 1);
+      expect(ppu.oam.inner.readByte(0)).toBe(0);
+      expect(ppu.oam.readByte(0)).toBe(0xff);
+    });
+
+    test('during HBlank', () => {
+      let ppu = new PPU();
+
+      ppu.step(Constants.HBLANK_START);
+      expect(ppu.registers.values.stat & 0b11).toEqual(PPUMode.HBlank);
+
+      // RAM is unlocked
+      expect(ppu.ram.readByte(0)).toBe(0);
+      ppu.ram.writeByte(0, 1);
+      expect(ppu.ram.readByte(0)).toBe(1);
+
+      // OAM is unlocked
+      expect(ppu.oam.readByte(0)).toBe(0);
+      ppu.oam.writeByte(0, 1);
+      expect(ppu.oam.readByte(0)).toBe(1);
+    });
+
+    test('during VBlank', () => {
+      let ppu = new PPU();
+
+      ppu.step(Constants.LINE_DURATION * Constants.VISIBLE_LINES);
+      expect(ppu.registers.values.stat & 0b11).toEqual(PPUMode.VBlank);
+
+      // RAM is unlocked
+      expect(ppu.ram.readByte(0)).toBe(0);
+      ppu.ram.writeByte(0, 1);
+      expect(ppu.ram.readByte(0)).toBe(1);
+
+      // OAM is unlocked
+      expect(ppu.oam.readByte(0)).toBe(0);
+      ppu.oam.writeByte(0, 1);
+      expect(ppu.oam.readByte(0)).toBe(1);
+    });
+  });
+});
