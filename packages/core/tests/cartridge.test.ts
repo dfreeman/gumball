0baee8d76a453f172f6a72c689d56d4974a7d9e3
Dan Freeman <dfreeman@salsify.com>
2026-02-23 21:45:33 +0000

    Tests that call out to RGBDS can run concurrently

--- packages/core/tests/cartridge.test.ts
+++ packages/core/tests/cartridge.test.ts
@@ -3,7 +3,7 @@ import { asm } from '#tests/helpers/compile-rom';
 import Cartridge, { CGBSupport, SGBSupport, MBCType } from '#src/cartridge';
 
 describe('Cartridge', () => {
-  test('loading a simple ROM', async () => {
+  test.concurrent('loading a simple ROM', async () => {
     let { rom } = await asm`
       SECTION "Header", ROM0[$100]
     `;
@@ -19,7 +19,7 @@ describe('Cartridge', () => {
     });
   });
 
-  test('with CGB and SGB support', async () => {
+  test.concurrent('with CGB and SGB support', async () => {
     let { rom } = await asm({ cgbSupport: CGBSupport.Required, sgbSupport: SGBSupport.Supported })`
       SECTION "Header", ROM0[$100]
     `;
@@ -30,7 +30,7 @@ describe('Cartridge', () => {
     });
   });
 
-  test('with invalid header checksum', async () => {
+  test.concurrent('with invalid header checksum', async () => {
     let { rom } = await asm({ setHeaderChecksum: false })`
       SECTION "Header", ROM0[$100]
     `;
@@ -38,7 +38,7 @@ describe('Cartridge', () => {
     expect(new Cartridge(rom).hasValidHeader()).toBe(false);
   });
 
-  test('with invalid logo bytes', async () => {
+  test.concurrent('with invalid logo bytes', async () => {
     let { rom } = await asm({ setNintendoLogo: false })`
       SECTION "Header", ROM0[$100]
     `;
